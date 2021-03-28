dacbca1c361c9850118f00406e160b8bebde2254
addas8282 <69339400+addas8282@users.noreply.github.com>
2021-03-28 19:14:36 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = "#"
+const express = require("express");
+const app = express();
+app.listen(() => console.log());
+require('events').EventEmitter.defaultMaxListeners = 30;
+app.use('/', (req, res) => {
+  res.send(`The BOt Is Now Online `);
+});
+console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+console.log('         [Wait please .. ]       ')
+console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+client.on('ready', () => {
+    console.log('')
+    console.log('')
+    console.log('')
+    console.log('')
+    console.log('')
+    console.log('')
+    console.log('')
+    console.log('')
+  console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+  console.log(`Logged in as [ ${client.user.tag}! ]`);
+  console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+  console.log('[           BOT IS ONLINE         ]')
+  console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+  console.log('[        info         ]')
+  console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+  console.log(`servers! [ " ${client.guilds.cache.size} " ]`);
+  console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+  console.log(`Users! [ " ${client.users.cache.size} " ]`);
+  console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+  console.log(`channels! [ " ${client.channels.cache.size} " ]`);
+  console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+ 
+client.user.setActivity('3RB', { type: 'PLAYING' })
+});
+
+client.login('NzMwNzM5MzY3NjY0MDI1NjUw.Xwb4EQ.Ve6pVm5CMr_YJi_ss8CVy8VwsaA');
+
+client.on("message", async message =>{
+let command = message.content.toLowerCase().split(" ")[0]
+command = command.slice(prefix.length)
+if (command == "avatar") {
+let args = message.content.split(" ")
+let user = message.mentions.users.first() || message.author || message.guild.member.cache.get(args[1])
+message.channel.send( new Discord.MessageEmbed()
+.setAuthor(user.username,user.avatarURL())
+.setDescription(`**[Avatar Link](${user.avatarURL()})**`)
+.setImage(user.avatarURL(
+{dynamic : true,
+format : 'png',
+size : 1024}
+))
+);
+}
+});
+
+
